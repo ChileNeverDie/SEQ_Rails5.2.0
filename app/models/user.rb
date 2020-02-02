165aedbf26d7c337b38cc7a0dfd11435e045da1d
Chile <928572034@qq.com>
2020-02-02 15:43:21 +0000

    Add basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   validates :title, presence: true, length: {maximum: 20}, inclusion: {in:['SQE','Senior SQE','Intern'],message:'Must be one of "Intern", "SQE", "Senior SQE"'} 
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true 
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
